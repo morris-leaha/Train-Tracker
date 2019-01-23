5c911ab7c2f091efb9eabc0c6d5f81363f5b1e99
LAMorris <morris.leaha@gmail.com>
2019-01-23 17:52:52 -0600

    started pseudocoding and updated ids for form input on html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,4 +12,21 @@
   // Create a variable to reference the database.
   var database = firebase.database();
 
-  
+  // Create initial variables 
+  var name;
+  var destination;
+  var time;
+  var frequency;
+
+  // Create an on click event tied to the submit button
+    // Utilize the event.preventDefault() method bc of submit button
+    // Grab values from input fields and store in respective global variables
+    // Push these values into firebase route directory 
+    // clear text input fields from input form
+
+
+  // Create an event listener function that allows new info input to be added to database and written to DOM when user adds
+    // console.log the 'snapshot'
+        // store each value in its own variable
+    // write respective train values to table 
+        // handle the errors 
